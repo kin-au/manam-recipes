2ff01e3f2ec97fb3135b4949c03777c2fa0b7d73
kin-au <kinnamau@gmail.com>
2021-04-26 23:49:46 +0100

    add basic header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-function App() {
-  return <div>Manam recipes</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>by Kin</p>
+        <a href="https://kinau.co.uk" target="_blank" rel="noopener noreferrer">
+          kinau.co.uk
+        </a>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
