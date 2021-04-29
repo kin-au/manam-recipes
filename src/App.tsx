874a00f07f9ff7eab9618557634b313ed598f4c3
kin-au <kinnamau@gmail.com>
2021-04-29 01:11:35 +0100

    add temp data and fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 const App = () => {
+  const getRecipes = () => {
+    fetch("./recipes.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((recipes) => console.log(recipes));
+  };
   return (
     <>
       <Header />
