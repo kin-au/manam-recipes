266f598e9ee713edbc1cb63521f160aa7982fc50
kin-au <kinnamau@gmail.com>
2021-05-02 23:51:29 +0100

    update types and recipes

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,13 @@
 export interface Recipe {
   name: string;
   description: string;
+  cuisine: string;
+  type: string;
+  servings: string;
   time: number;
   ingredients: string[];
   steps: string[];
+  source: string;
 }
 
 export type RecipeData = any | Recipe[];
