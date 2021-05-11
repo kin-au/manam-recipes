bea513f7f77383022c99cac5789b586dba804e21
kin-au <kinnamau@gmail.com>
2021-05-11 01:07:17 +0100

    update header style

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 const Header = styled.header`
-  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+  height: 6rem;
+  width: 100%;
+  background-color: #e5a4cb;
+  color: #403d39;
+  /* color: #FFFCF2; */
+  font-size: 3rem;
 `;
 
 export { Header };
