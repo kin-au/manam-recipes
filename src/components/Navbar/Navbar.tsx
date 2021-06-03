7f73dddb6eb83e78f15629046942f401add3764c
kin-au <kinnamau@gmail.com>
2021-06-03 22:46:31 +0100

    refactor mui navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,102 +1,57 @@
 import React from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import Divider from "@material-ui/core/Divider";
 
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: "auto",
-  },
-});
-
-type Anchor = "top" | "left" | "bottom" | "right";
-
-export default function SwipeableTemporaryDrawer() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event &&
-        event.type === "keydown" &&
-        ((event as React.KeyboardEvent).key === "Tab" ||
-          (event as React.KeyboardEvent).key === "Shift")
-      ) {
-        return;
-      }
-
-      setState({ ...state, [anchor]: open });
-    };
-
-  const list = (anchor: Anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {/* change to array of objects containing text and link/href */}
-        {/* add onclick to each listitem to change route/page */}
-        {["Home", "About", "Recipes", "Contact"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {/* logic to display different icons */}
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["Instagram"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+const Navbar = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
 
   return (
     <div>
-      {(["left"] as Anchor[]).map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
+      <IconButton
+        aria-label="open navbar"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SwipeableDrawer
+        anchor="top"
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+        onOpen={() => {}}
+      >
+        <div>
+          <Box textAlign="center" p={6}>
+            Menu
+          </Box>
+          <Divider />
+          <List>
+            <ListItem button onClick={() => {}}>
+              <ListItemText primary={"Home"} />
+            </ListItem>
+            <ListItem button onClick={() => {}}>
+              <ListItemText primary={"About"} />
+            </ListItem>
+            <ListItem button onClick={() => {}}>
+              <ListItemText primary={"Recipes"} />
+            </ListItem>
+            <ListItem button onClick={() => {}}>
+              <ListItemText primary={"Contact"} />
+            </ListItem>
+          </List>
+        </div>
+      </SwipeableDrawer>
     </div>
   );
-}
+};
+
+export default Navbar;
