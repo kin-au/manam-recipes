5b4533c7dec7191140ce280cc66c3e7d1110bfa2
kin-au <kinnamau@gmail.com>
2021-06-07 23:53:06 +0100

    add page links and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import * as SC from "./App.style";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 
 const App = () => {
   const getRecipes = () => {
@@ -15,11 +19,31 @@ const App = () => {
   };
   return (
     <>
-      <SC.GlobalStyle />
-      <SC.Container>
-        <Header />
-        <Footer />
-      </SC.Container>
+      <BrowserRouter>
+        <SC.GlobalStyle />
+        <SC.Container>
+          <Header />
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/recipes">Recipes</Link>
+          <Link to="/contact">Contact</Link>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/recipes">
+              <Recipes />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </SC.Container>
+      </BrowserRouter>
     </>
   );
 };
