a32ca4e49840f3730b799d3302199eb18a92d2c2
kin-au <kinnamau@gmail.com>
2021-06-28 22:03:28 +0100

    init recipe finder component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import * as SC from "./Header.style.js";
 import Navbar from "../Navbar/Navbar";
+import RecipeFinder from "../RecipeFinder/RecipeFinder";
 
 const Header = () => {
   return (
     <SC.Header>
       <h1>Manam recipes</h1>
       <Navbar />
+      <RecipeFinder />
     </SC.Header>
   );
 };
---
+++ src/components/RecipeFinder/RecipeFinder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecipeFinder = () => {
+  return (
+    <>
+      <input type="text" placeholder="Search by recipe name"></input>
+    </>
+  );
+};
+
+export default RecipeFinder;
