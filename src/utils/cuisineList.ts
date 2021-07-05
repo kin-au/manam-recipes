f1d08b617fc76671da4be065f50352d54b2ee50d
kin-au <kinnamau@gmail.com>
2021-07-05 23:54:08 +0100

    cuisine list getter

---
+++ src/utils/cuisineList.ts
@@ -0,0 +1,8 @@
+import { AllRecipeData, RecipeData } from "../types";
+
+const cuisineList = (allRecipes: AllRecipeData): string[] => {
+  const cuisineList = allRecipes.map((recipe: RecipeData) => recipe.cuisine);
+  return Array.from(new Set(cuisineList));
+};
+
+export default cuisineList;
