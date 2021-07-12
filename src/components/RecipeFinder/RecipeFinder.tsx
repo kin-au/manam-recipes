6eca415825ca4cb2baa2c44de4411272e5155a30
kin-au <kinnamau@gmail.com>
2021-07-13 00:02:27 +0100

    reset sort with filters

--- src/components/RecipeFinder/RecipeFinder.tsx
+++ src/components/RecipeFinder/RecipeFinder.tsx
@@ -35,6 +35,8 @@ const RecipeFinder = (props: RecipeFinderProps) => {
     setNameSearchText("");
     setCuisineSearchText("");
     setTypeSearchText("");
+    setSortType("Alphabetical");
+    setSortOrder("Ascending");
   };
 
   return (
