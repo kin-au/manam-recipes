99bcc6e0280e40da445cafaaf5d0691eafaa5139
kin-au <kinnamau@gmail.com>
2021-07-17 09:47:51 +0100

    init recipefinder style

---
+++ src/components/RecipeFinder/RecipeFinder.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const RecipeFinder = styled.div``;
+
+const SearchContainer = styled.div``;
+
+const SortContainer = styled.div``;
+
+export { RecipeFinder, SearchContainer, SortContainer };
--- src/components/RecipeFinder/RecipeFinder.tsx
+++ src/components/RecipeFinder/RecipeFinder.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import * as SC from "./RecipeFinder.style";
 import allSearch from "../../utils/allSearch";
 import cuisineList from "../../utils/cuisineList";
 import sortRecipes from "../../utils/sortRecipes";
@@ -41,58 +42,62 @@ const RecipeFinder = (props: RecipeFinderProps) => {
 
   return (
     <>
-      <label htmlFor="nameSearch">Search recipes</label>
-      <input
-        id="nameSearch"
-        type="search"
-        placeholder="Find a recipe by name"
-        value={nameSearchText}
-        onChange={(event) => setNameSearchText(event.target.value)}
-      />
-      <label htmlFor="cuisineSearch">Search cuisines</label>
-      <select
-        id="cuisineSearch"
-        value={cuisineSearchText}
-        onChange={(event) => setCuisineSearchText(event.target.value)}
-      >
-        <option value="">All cuisines</option>
-        {cuisineList(allRecipes).map((cuisine) => {
-          return <option value={cuisine}>{cuisine}</option>;
-        })}
-      </select>
-      <label htmlFor="typeSearch">Search types</label>
-      <select
-        id="typeSearch"
-        value={typeSearchText}
-        onChange={(event) => setTypeSearchText(event.target.value)}
-      >
-        <option value="">All types</option>
-        {typeList(allRecipes).map((type) => {
-          return <option value={type}>{type}</option>;
-        })}
-      </select>
-      <label htmlFor="sortType">Sort by</label>
-      <select
-        id="sortType"
-        value={sortType}
-        onChange={(event) => setSortType(event.target.value as SortType)}
-      >
-        <option value="Alphabetical">Alphabetical</option>
-        <option value="Prep & cook time">Prep & cook time</option>
-        <option value="Number of ingredients">Number of ingredients</option>
-      </select>
-      <label htmlFor="sortOrder">Sort direction</label>
-      <select
-        id="sortOrder"
-        value={sortOrder}
-        onChange={(event) => setSortOrder(event.target.value as SortOrder)}
-      >
-        <option value="Ascending">Ascending</option>
-        <option value="Descending">Descending</option>
-      </select>
-      <button type="button" onClick={() => resetSearch()}>
-        Reset
-      </button>
+      <SC.SearchContainer>
+        <label htmlFor="nameSearch">Search recipes</label>
+        <input
+          id="nameSearch"
+          type="search"
+          placeholder="Find a recipe by name"
+          value={nameSearchText}
+          onChange={(event) => setNameSearchText(event.target.value)}
+        />
+        <label htmlFor="cuisineSearch">Search cuisines</label>
+        <select
+          id="cuisineSearch"
+          value={cuisineSearchText}
+          onChange={(event) => setCuisineSearchText(event.target.value)}
+        >
+          <option value="">All cuisines</option>
+          {cuisineList(allRecipes).map((cuisine) => {
+            return <option value={cuisine}>{cuisine}</option>;
+          })}
+        </select>
+        <label htmlFor="typeSearch">Search types</label>
+        <select
+          id="typeSearch"
+          value={typeSearchText}
+          onChange={(event) => setTypeSearchText(event.target.value)}
+        >
+          <option value="">All types</option>
+          {typeList(allRecipes).map((type) => {
+            return <option value={type}>{type}</option>;
+          })}
+        </select>
+      </SC.SearchContainer>
+      <SC.SortContainer>
+        <label htmlFor="sortType">Sort by</label>
+        <select
+          id="sortType"
+          value={sortType}
+          onChange={(event) => setSortType(event.target.value as SortType)}
+        >
+          <option value="Alphabetical">Alphabetical</option>
+          <option value="Prep & cook time">Prep & cook time</option>
+          <option value="Number of ingredients">Number of ingredients</option>
+        </select>
+        <label htmlFor="sortOrder">Sort direction</label>
+        <select
+          id="sortOrder"
+          value={sortOrder}
+          onChange={(event) => setSortOrder(event.target.value as SortOrder)}
+        >
+          <option value="Ascending">Ascending</option>
+          <option value="Descending">Descending</option>
+        </select>
+        <button type="button" onClick={() => resetSearch()}>
+          Reset
+        </button>
+      </SC.SortContainer>
     </>
   );
 };
