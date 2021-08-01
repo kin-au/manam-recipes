af9e06e7e4bcc41201f2886ecdde42db409118e7
kin-au <kinnamau@gmail.com>
2021-08-01 23:52:21 +0100

    link to return to recipe search

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { RecipeProps } from "./types";
 import DOMPurify from "dompurify";
 
@@ -42,6 +43,7 @@ const Recipe = (props: RecipeProps) => {
 
   return (
     <>
+      <Link to={"/recipes"}>Back to search results</Link>
       <header>{recipe.name}</header>
       <div>{recipe.description}</div>
       <div>
