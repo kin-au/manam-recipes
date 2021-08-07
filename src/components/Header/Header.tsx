73fb18dba5534826b67f10cf75f3167963a38d6b
kin-au <kinnamau@gmail.com>
2021-08-08 00:09:34 +0100

    conditional render search and filter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,18 @@ import * as SC from "./Header.style.js";
 import { HeaderProps } from "./types";
 import Navbar from "../Navbar/Navbar";
 import RecipeFinder from "../RecipeFinder/RecipeFinder";
+import { useLocation } from "react-router-dom";
 
 const Header = (props: HeaderProps) => {
   const { allRecipes, setShowRecipes } = props;
+  const showingRecipes = useLocation().pathname === "/recipes";
   return (
     <SC.Header>
       <h1>Manam recipes</h1>
       <Navbar />
-      <RecipeFinder allRecipes={allRecipes} setShowRecipes={setShowRecipes} />
+      {!showingRecipes ? null : (
+        <RecipeFinder allRecipes={allRecipes} setShowRecipes={setShowRecipes} />
+      )}
     </SC.Header>
   );
 };
